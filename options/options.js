a5cbf7e66c414a73ae753fe4be7a77e6b40f1e1c
Raj sharma <kevinrinta2018@gmail.com>
2023-12-27 11:22:07 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "rahultest1-45199",
   url: "https://rahultest1-45199.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
--- screens/welcome/index.js
+++ screens/welcome/index.js
@@ -1,64 +1,23 @@
-import React from "react"
-import {
-  View,
-  Image,
-  Text,
-  ScrollView,
-  SafeAreaView,
-  StyleSheet
-} from "react-native"
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, View, Text, FlatList } from 'react-native';
+const API_URL = 'https://api.publicapis.org/entries';
 
-const WelcomeScreen = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView contentContainerStyle={styles.scrollView}>
-        <View style={styles.group} />
-        <View style={styles.group}>
-          <Image style={styles.logo} source={require("./logo.png")} />
-          <Text style={styles.text}>
-            Let's build something amazing together!
-          </Text>
-        </View>
-        <Text style={styles.footer}>Made with ❤️ by Crowdbotics</Text>
-      </ScrollView>
-    </SafeAreaView>
-  )
-}
+const ListScreen = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch(API_URL).then(response => response.json()).then(json => setData(json.entries)).catch(error => console.error(error));
+  }, []);
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#F8F8FC",
-    height: "100%"
-  },
-  scrollView: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "space-between",
-    padding: 20
-  },
-  group: {
-    alignItems: "center"
-  },
-  logo: {
-    height: 180,
-    width: 180,
-    padding: 40,
-    borderRadius: 30,
-    margin: 40
-  },
-  text: {
-    textAlign: "center",
-    fontSize: 28,
-    color: "#828AB0",
-    fontWeight: 700
-  },
-  footer: {
-    textAlign: "center",
-    fontSize: 18,
-    color: "#828AB0",
-    fontWeight: 700,
-    marginBottom: 20
-  }
-})
+  const renderItem = ({
+    item
+  }) => <View>
+      <Text>{item.API}</Text>
+      <Text>{item.Description}</Text>
+    </View>;
 
-export default WelcomeScreen
+  return <SafeAreaView>
+      <FlatList data={data} renderItem={renderItem} keyExtractor={item => item.API} />
+    </SafeAreaView>;
+};
+
+export default ListScreen;
